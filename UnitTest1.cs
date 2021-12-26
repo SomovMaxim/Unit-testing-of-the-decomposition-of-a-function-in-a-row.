319beaa9cf9667db5a1db7709b8b9997c65e78d5
SomovMaxim <71885497+SomovMaxim@users.noreply.github.com>
2021-12-26 23:00:19 +0200

    Add files via upload

---
+++ UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using ConsoleApp2;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            double result = Math.Round(Math.Acos(30 * Math.PI / 180),4);
+            double resultMy = Math.Round(Math.PI / 2 - ConsoleApp2.Program.ArcSin(30*Math.PI/180),4);
+            Assert.AreEqual(result, resultMy);
+        }
+    }
+}
